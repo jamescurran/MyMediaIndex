d84503817a8109f45585d09e75d9f596898fc39c
James Curran <james.curran@gmail.com>
2024-11-27 13:37:51 -0500

    Initial checkin

---
+++ MyMediaIndex/Program.cs
@@ -0,0 +1,194 @@
+﻿using System.Diagnostics;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+
+using System.Web;
+
+
+namespace MyMediaIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+	        // Ensures index backward compatibility
+	        const LuceneVersion AppLuceneVersion = LuceneVersion.LUCENE_48;
+
+	        // Construct a machine-independent path for the index
+//	        var basePath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+//	        var basePath = @"C:\Temp\MyMediaTest";
+	        var basePath = @"C:\Program Files\MyMediaForAlexa";
+	        var indexPath = Path.Combine(basePath, "Index_test");
+
+	        using var dir = FSDirectory.Open(indexPath);
+
+	        // Create an analyzer to process the text
+	        var analyzer = new StandardAnalyzer(AppLuceneVersion);
+
+	        // Create an index writer
+	        var indexConfig = new IndexWriterConfig(AppLuceneVersion, analyzer);
+	        using var writer = new IndexWriter(dir, indexConfig);
+
+
+			///////////////////////
+			//var source = new
+			//{
+			//	Name = "Third  Item",
+			//	FavoritePhrase = "Also Contains none  of the words",
+			//	OtherField = "Some other value"
+			//};
+			//var doc = new Document
+			//{
+			//	// StringField indexes but doesn't tokenize
+			//	new StringField("name",
+			//		source.Name,
+			//		Field.Store.YES),
+			//	new TextField("favoritePhrase",
+			//		source.FavoritePhrase,
+			//		Field.Store.YES),
+			//	new TextField("otherField",
+			//		source.OtherField,
+			//		Field.Store.YES)
+			//};
+
+			//writer.AddDocument(doc);
+			//writer.Flush(triggerMerge: false, applyAllDeletes: false);
+
+
+			//////////////
+			// Search with a phrase
+			var phrase = new MultiPhraseQuery
+			{
+				new Term("favoritePhrase", "brown"),
+				new Term("favoritePhrase", "fox")
+			};
+
+			//////////////////
+
+			// Re-use the writer to get real-time updates
+			using var reader = writer.GetReader(applyAllDeletes: true);
+			var searcher = new IndexSearcher(reader);
+//			var hits = searcher.Search(phrase, 20 /* top 20 */).ScoreDocs;
+
+	        var ts = Stopwatch.GetTimestamp();
+
+			var maxDoc = searcher.IndexReader.MaxDoc;
+			var songs = new List<Song>(maxDoc);
+			var Albums = new List<Album>(maxDoc/10);
+			var Artists = new List<Artist>(maxDoc/5);
+			var genres = new List<Genre>(maxDoc / 90);
+
+			for (int hit = 0; hit < searcher.IndexReader.MaxDoc; hit++)
+			{
+				var foundDoc = searcher.Doc(hit);
+				if (foundDoc == null) break;
+				var docType = foundDoc.GetDocumentType();
+				switch(docType)
+				{
+					case DocumentTypes.Album:
+						Albums.Add(foundDoc.ReadAlbum());
+						break;
+					case DocumentTypes.Artist:
+						Artists.Add(foundDoc.ReadArtist());
+						break;
+					case DocumentTypes.Song:
+						songs.Add(foundDoc.ReadTrack());
+						break;
+					case DocumentTypes.Genre:
+						genres.Add(foundDoc.ReadGenre());
+						break;
+					default:
+						foreach (var field in foundDoc.Fields)
+							Console.WriteLine(field);
+						break;
+					
+				}
+				//Console.WriteLine($" {hits[hit].Score,10} {foundDoc.Get("name"),-15} {foundDoc.Get("favoritePhrase"),-40}");
+				
+			}
+			var elapsed = Stopwatch.GetElapsedTime(ts);
+
+			var min = songs.Min(song => song.DateIndexed);
+	        var max = songs.Max(song => song.DateIndexed);
+
+
+#if false
+			var played = songs.OrderByDescending(song => song.Genre.Count > 1).ToList();
+			foreach (var song in played.Take(10))
+			{
+				Console.WriteLine(song);
+				Console.WriteLine(String.Join(", ", song.Genre));
+			}
+			Console.WriteLine($"Played: {played.Count}");
+#endif
+
+#if true
+#if true
+			var bSongs = songs.Where(song =>
+			{
+				var fi = new FileInfo(song.Filename);
+				return fi.CreationTime > DateTime.Now.AddDays(-160);
+			}		
+			).ToList();
+	        WritePlayList(bSongs, "New Stuff");
+
+#endif
+#if true
+			 bSongs = songs.Where(song =>
+			{
+				var fi = new FileInfo(song.Filename);
+				return fi.CreationTime > DateTime.Now.AddYears(-1) && song.PlayCount < 5;
+			}		
+			).ToList();
+	        var plName = ;
+#endif
+
+			//var groups =songs.GroupBy(song=> song.PlayCount).ToList();
+
+			WritePlayList(bSongs, "Unplayed Recent");
+#endif
+
+
+			Console.WriteLine(elapsed);
+		}
+
+        private static void WritePlayList(List<Song> songs, string plName)
+        {
+	        var totalDuration = songs.Sum(song=>song.Duration.TotalSeconds);
+	        var plFile = Path.Combine(@"M:\Music\Playlists", Path.ChangeExtension(plName, ".wpl"));
+	        using (var tw = File.CreateText(plFile))
+	        {
+
+		        tw.WriteLine($"""
+		                      <?wpl version="1.0"?>
+		                        <smil>
+		                            <head>
+		                                <meta name="Generator" content="MyMediaIndex"/>
+		                                <meta name="TotalDuration" content="{totalDuration}"/>
+		                                <meta name="ItemCount" content="{songs.Count}"/>
+		                                <author/>
+		                                <title>{plName}</title>
+		                            </head>
+		                            <body>
+		                            <seq>
+		                      """);
+
+		        foreach (var song in songs)
+		        {
+			        tw.WriteLine($"\t<media src=\"{HttpUtility.HtmlEncode(song.Filename)}\" tid=\"{song.Guid}\" />");
+		        }
+		        tw.Write($"""
+		                        </seq>
+		                       </body>
+		                  </smil>
+
+		                  """);
+
+	        }
+        }
+    }
+}
---
+++ MyMediaIndex/Song.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Documents;
+
+namespace MyMediaIndex
+{
+	public class Song
+	{
+		public string? Album { get; set; }
+		public string Title { get; set; }
+		public string? Artist { get; set; }
+		public string? Filename { get; set; }
+		public string? Filename_na { get; set; }
+		public string? Checksum { get; set; } //type: NONE, value: -8586020546919179825:16089475
+		public Guid Guid { get; set; } //type: NONE, value: 5eac2292-861e-41ca-ba57-d7564c2d2d52
+		public int DiscNumber { get; set; } //type: NONE, value: 0
+		public int TrackNumber { get; set; } //type: NONE, value: 5
+		public DateTime DateIndexed { get; set; } //type: NONE, value: 20241123194347
+		public string? Codec { get; set; } //type: NONE, value: MPEG Version 1 Audio, Layer 3, mp3tagatend
+		public TimeSpan Duration { get; set; } //type: NONE, value: 400
+		public int PlayCount { get; set; } //type: NONE, value: 0
+		public List<string> Genre { get; set; } = new List<string>(); 
+		
+		public override string ToString() => $"{Title} - {Artist}- {Album}";
+	}
+
+
+
+	public class Album : ITrait
+	{
+		public Guid? Guid { get; set; } // type: NONE, value: 44cfdc2d-a74e-43d1-9748-44edd6895ae9
+		public string Name { get; set; } // type: NONE, value: Live at Wonder Ballroom
+		public string Name_na { get; set; }// type: NONE, value: live at wonder ballroom
+		public int TrackCount { get; set; }// type: NONE, value: 1
+		public Guid? FirstGuid { get; set; }// type: NONE, value: 5eac2292-861e-41ca-ba57-d7564c2d2d52
+		public DateTime? DateIndexed { get; set; }//type: NONE, value: 20241123194347}
+		
+		public override string ToString() => $"Album: {Name}";
+	}
+
+
+	public class Genre : ITrait
+	{
+		public Guid? Guid { get; set; } // type: NONE, value: 44cfdc2d-a74e-43d1-9748-44edd6895ae9
+		public string Name { get; set; } // type: NONE, value: Live at Wonder Ballroom
+		public string Name_na { get; set; }// type: NONE, value: live at wonder ballroom
+		public int TrackCount { get; set; }// type: NONE, value: 1
+		public Guid? FirstGuid { get; set; }// type: NONE, value: 5eac2292-861e-41ca-ba57-d7564c2d2d52
+		public DateTime? DateIndexed { get; set; }//type: NONE, value: 20241123194347}
+
+		public override string ToString() => $"Genre: {Name}";
+	}
+
+	public interface ITrait
+	{
+		Guid? Guid { get; set; } // type: NONE, value: 44cfdc2d-a74e-43d1-9748-44edd6895ae9
+		string Name { get; set; } // type: NONE, value: Live at Wonder Ballroom
+		string Name_na { get; set; } // type: NONE, value: live at wonder ballroom
+		int TrackCount { get; set; } // type: NONE, value: 1
+		Guid? FirstGuid { get; set; } // type: NONE, value: 5eac2292-861e-41ca-ba57-d7564c2d2d52
+		DateTime? DateIndexed { get; set; } //type: NONE, value: 20241123194347}
+	}
+
+	public class Artist : ITrait
+	{
+		public Guid? Guid { get; set; } // type: NONE, value: 44cfdc2d-a74e-43d1-9748-44edd6895ae9
+		public string Name { get; set; } // type: NONE, value: Live at Wonder Ballroom
+		public string Name_na { get; set; }// type: NONE, value: live at wonder ballroom
+		public int TrackCount { get; set; }// type: NONE, value: 1
+		public Guid? FirstGuid { get; set; }// type: NONE, value: 5eac2292-861e-41ca-ba57-d7564c2d2d52
+		public DateTime? DateIndexed { get; set; }//type: NONE, value: 20241123194347}
+		public override string ToString() => $"Artist: {Name}";
+	}
+
+
+	public enum DocumentTypes
+	{
+			Unknown, Song, Album, Artist, Genre
+	}
+
+	public static class DocumentExt
+	{
+		public static DocumentTypes GetDocumentType(this Document doc)
+		{
+			if (doc.Get("album_guid") != null)
+			{
+				return DocumentTypes.Album;
+			}
+			else if (doc.Get("song") != null)
+			{
+				return DocumentTypes.Song;
+			}
+			else if (doc.Get("artist_guid") != null)
+			{
+				return DocumentTypes.Artist;
+			}
+			else if (doc.Get("genre_guid") != null)
+			{
+				return DocumentTypes.Genre;
+			}
+			else
+			{
+				return DocumentTypes.Unknown;
+			}
+		}
+		
+
+		public static Album ReadAlbum(this Document doc)
+		{
+			var album = new Album
+			{
+				Guid = Guid.Parse(doc.Get("album_guid")),
+				Name = doc.Get("album_name"),
+				Name_na = doc.Get("album_name_na"),
+				TrackCount = Int32.Parse( doc.Get("album_trackcount")),
+				FirstGuid = Guid.Parse( doc.Get("album_firstguid")),
+				DateIndexed = DateTime.ParseExact(doc.Get("album_dateindexed"), "yyyyMMddHHmmss", CultureInfo.CurrentCulture),
+			};
+			return album;
+		}
+
+
+		public static Artist ReadArtist(this Document doc)
+		{
+			var artist = new Artist
+			{
+				Guid = Guid.Parse(doc.Get("artist_guid")),
+				Name = doc.Get("artist_name"),
+				Name_na = doc.Get("artist_name_na"),
+				TrackCount = Int32.Parse(doc.Get("artist_trackcount")),
+				FirstGuid = Guid.Parse(doc.Get("artist_firstguid")),
+				DateIndexed = DateTime.ParseExact(doc.Get("artist_dateindexed"), "yyyyMMddHHmmss", CultureInfo.CurrentCulture),
+			};
+			return artist;
+		}
+
+
+		public static Genre ReadGenre(this Document doc)
+		{
+			var artist = new Genre
+			{
+				Guid = Guid.Parse(doc.Get("genre_guid")),
+				Name = doc.Get("genre_name"),
+				Name_na = doc.Get("genre_name_na"),
+				TrackCount = Int32.Parse(doc.Get("genre_trackcount")),
+				FirstGuid = Guid.Parse(doc.Get("genre_firstguid")),
+				DateIndexed = DateTime.ParseExact(doc.Get("genre_dateindexed"), "yyyyMMddHHmmss", CultureInfo.CurrentCulture),
+			};
+			return artist;
+		}
+
+		public static Song ReadTrack(this Document doc)
+		{
+			//if (doc.Fields.Count != 13+ doc.GetValues("genre").Length)
+			//	foreach (var field in doc.Fields)
+			//		Console.WriteLine(field);
+			
+			var song = new Song
+			{
+				Album = doc.Get("album"),
+				Title = doc.Get("song"),
+				Artist = doc.Get("artist"),
+				Filename = doc.Get("filename"),
+				Filename_na = doc.Get("filename_na"),
+				Checksum = doc.Get("checksum"),
+				Guid = Guid.Parse(doc.Get("guid")),
+				DiscNumber = Int32.Parse(doc.Get("discnumber")),
+				TrackNumber = Int32.Parse(doc.Get("tracknumber")),
+				DateIndexed = DateTime.ParseExact(doc.Get("dateindexed"), "yyyyMMddHHmmss", CultureInfo.CurrentCulture),
+				Codec = doc.Get("codec"),
+				Duration = TimeSpan.FromSeconds(Int32.Parse(doc.Get("duration"))),
+				PlayCount = Int32.Parse(doc.Get("playcount")),
+			};
+			song.Genre.AddRange(doc.GetValues("genre"));
+			return song;
+		}
+
+	}
+}
